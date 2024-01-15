a55f6f89a669a18d901304483611c52d4480aa9e
GrowContent <growcontent03@gmail.com>
2024-01-15 22:47:22 +0530

    Routes Changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import React from 'react'
+import * as React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './view/Home';
 import About from './view/About';
+import Findyourhomes from "./view/Findyourhomes";
 import Contact from './view/Contact';
-import Findyourhomes from './view/Findyourhomes';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "findyourhomes",
+    element: <Findyourhomes />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]);
 
 const Main = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/findyourhomes' element={<Findyourhomes />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+  <RouterProvider router={router} />
   )
 }
 
--- src/components/view/Findyourhomes.jsx
+++ src/components/view/Findyourhomes.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import HomeCard from './HomeCard'
+// import React, { useState ,useEffect} from 'react'
+import HomeCard from './HomeCard';
+
 
 const Findyourhomes = () => {
+  // const [getData , setData ] = useState('');
+  // const [visible, setVisible] = useState(2);
+   
+  // const fetchData = async() =>{
+  //  await fetch('https://jsonplaceholder.typicode.com/users').then((res)=> res.json()).then((data)=> setData(data));
+  // }
+  // const loadMore = ()=> {
+  //   setVisible(visible => visible + 2);
+  // }
+
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, [visible])
+  // if (getData) {
+  //   console.log(getData.length);
+  //   console.log(visible);
+  // }
+
+  
   return (
     <div id='findyourhomes'>
       <div className="home-container">
+        {/* {getData? 
+          getData.slice(0,visible).map((item,index)=>
+            <p key={index}>{item.name}</p>
+          )
+        : 'No Data'}
+        {visible < 10 ? <button onClick={loadMore}>Load More</button> : ""} */}
         <HomeCard />
         <HomeCard />
         <HomeCard />
